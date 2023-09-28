9a3c97fbdf2a001d4dc88142d6a56059cc9f8986
turkpenbayev <mr.turkpenbaev1997@gmail.com>
2023-09-28 14:49:51 +0500

    adedd admin migrations

--- src/adverts/admin.py
+++ src/adverts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from adverts.models import City, Category, Advert
 
-# Register your models here.
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Advert)
+class AdvertAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'views', 'created')
+    list_filter = ('city', 'category')
+    search_fields = ('title',)
+    ordering = ('title', 'city_id')
--- src/adverts/models.py
+++ src/adverts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self,):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=128)
+    def __str__(self,):
+        return self.name
+
+
+class Advert(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE, related_name='adverts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='adverts')
+    views = models.PositiveIntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self,):
+        return self.title
